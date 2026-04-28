6416f5c9c844eeef28854b30da82583ee1d5e555
Adil <asareu2@outlook.de>
2026-04-28 09:17:08 +0200

    first commit

--- MyProject.Client/src/App.tsx
+++ MyProject.Client/src/App.tsx
@@ -1,121 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import { Navbar } from './Components/Navbar';
+import Dashboard from './pages/Dashboard';
+import { ChallengeDetail } from './pages/ChallengeDetail';
+import { AddChallenge } from './Components/AddChallenge';
+import { useState } from 'react';
+import type { Challenge } from './types';
+import { History } from './pages/History';
+
+const APP_API_URL = 'The Api Frontend';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [challenges, setChallenges] = useState<Challenge[]>([]);
 
+  const handleAddChallenge = (newCh: Omit<Challenge, 'id'>) => {
+    const challengeWithId = { ...newCh, id: Math.random() }; // Temporäre ID
+    setChallenges((prev) => [...prev, challengeWithId]); // State aktualisieren
+  };
+
+  const updateChallengeDays = (id: number) => {
+    setChallenges((prev) =>
+      prev.map((c) =>
+        c.id === id
+          ? { ...c, daysActive: Math.min(c.daysActive + 1, c.goalDays) }
+          : c,
+      ),
+    );
+  };
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
 
-      <div className="ticks"></div>
+          <main className="max-w-7xl mx-auto py-6">
+            <Routes>
+              <Route path="/" element={<Dashboard challenges={challenges} />} />
+              <Route
+                path="/challenge/:id"
+                element={
+                  <ChallengeDetail
+                    challenges={challenges}
+                    onUpdate={updateChallengeDays}
+                  />
+                }
+              />
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+              <Route path="/history" element={<History />} />
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+              <Route
+                path="/add"
+                element={<AddChallenge onAdd={handleAddChallenge} />}
+              />
+            </Routes>
+          </main>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
